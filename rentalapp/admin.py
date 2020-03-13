d1cf22fd9d66863f9ff13c6271e196454f594dbe
SalamanderKrajza <salamanderkrajza@gmail.com>
2020-03-13 19:52:53 +0100

    models created

--- rentalapp/admin.py
+++ rentalapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Client, Author, Book, Category, CategoryFilter
+
+admin.site.register(Client)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(CategoryFilter)
--- rentalapp/models.py
+++ rentalapp/models.py
@@ -25,7 +25,41 @@ class Book(models.Model):
     title = models.CharField(max_length=30)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     status = models.CharField(max_length=30)
+    ordered = models.BooleanField()
     
     def __str__(self):
         return f'{self.Title} by {self.author}'
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f'{self.name}'
+
+class Orders(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=False)
+    operation_date = models.DateField()
+    start_date = models.DateField()
+    expected_end_date = models.DateField()
+    expanded_end_date = models.DateField()
+    end_date = models.DateField()
+
+    #Posible status which could be achieved by each order
+    reserved = models.BooleanField()
+    canceled = models.BooleanField()
+    rented = models.BooleanField()
+    delayed = models.BooleanField()
+    expanded = models.BooleanField()
+    returned = models.BooleanField()
+
+    #actual status of ordered item
+    status = models.CharField(max_length=30)
+
+    
+    def __str__(self):
+        return f'{self.Title} by {self.author}'
+
+class CategoryFilter(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=False)
