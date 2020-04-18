7edd0166044c607b1576ac53879ee82f18e856ce
SalamanderKrajza <salamanderkrajza@gmail.com>
2020-04-18 21:08:44 +0200

    update admin.py

--- app/rentalapp/admin.py
+++ app/rentalapp/admin.py
@@ -3,9 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Book, Category, CategoryFilter, Order, Action
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'is_reserved', 'is_avaiable')
+    list_filter = ('is_reserved', 'is_avaiable')
+    search_fields = ('title', 'author__name', 'author__surname')
+
+class OrderInline(admin.TabularInline):
+    model = Order
+
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('action', 'action')
+    inlines = [
+        OrderInline,
+    ]
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Category)
 admin.site.register(CategoryFilter)
 admin.site.register(Order)
-admin.site.register(Action)
+admin.site.register(Action, ActionAdmin)
+
+
+
